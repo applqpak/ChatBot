82adb6653eae7f7c5f0658302c0f6642f8d2a470
applqpak <applqpak@gmail.com>
2016-04-12 09:34:02 +1000

    Update Main.php

--- src/ChatBot/Main.php
+++ src/ChatBot/Main.php
@@ -11,10 +11,68 @@
   class Main extends PluginBase implements Listener
   {
 
+    // Create PREFIX constant
+
+    const PREFIX = TF::RED . "[" . TF::AQUA . "ChatBot" . TF::RED . "] " . TF::RESET;
+
+    // Our own functions
+
     public function dataPath()
     {
 
       return $this->getDataFolder();
+
+    }
+
+    public function server()
+    {
+
+      return $this->getServer();
+
+    }
+
+    public function logger()
+    {
+
+      return $this->server()->getLogger();
+
+    }
+
+    public function pluginManager()
+    {
+
+      return $this->server()->getPluginManager();
+
+    }
+
+    // OK, this plugin was enabled!
+
+    public function onEnable()
+    {
+
+      @mkdir($this->dataPath());
+
+      $this->cfg = new Config($this->dataPath() . "config.yml", Config::YAML, array("messages" => array("hello" => "Hello, %p!")));
+
+      $this->server()->logger()->info(PREFIX . "Enabled.");
+
+    }
+
+    // OK, this plugin was disabled, make sure to save the Config!
+
+    public function onDisable()
+    {
+
+      $this->cfg->save();
+
+      $this->server()->logger()->info(PREFIX . "Disabled. Saved Config.");
+
+    }
+
+    // OK, this function is called Everytime a player chats.
+
+    public function onChat(PlayerChatEvent $event)
+    {
 
     }
 
